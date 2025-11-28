3cdc5f12474b8f08c2275b6874da9faabe52db93
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-11-29 00:06:22 +0100

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'EL AMRANI SARA',
+    author_email= 'saraelamrani269@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
